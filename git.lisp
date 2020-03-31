62c2f2a6e7f7247b36a6c8cd55b3d72eb509ceb0
goo <goofofferson@gmail.com>
2020-03-31 13:02:06 -0400

    documentation

--- git.lisp
+++ git.lisp
@@ -3,7 +3,9 @@
 
 (named-readtables:in-readtable :interpol-syntax)
 
-(defun update-all ()
+(defun git-update-all ()
+  "Does a git pull with --recurse-submodules and update --remote.  Brings all submodules up to date"
+  #+nil(eazy-process:exec `("git" "pull"))
   (eazy-process:exec `("git" "pull" "--recurse-submodules"))
   (eazy-process:exec `("git" "submodule" "update" "--remote"))
   )

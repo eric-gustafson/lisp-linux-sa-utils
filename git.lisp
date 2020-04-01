20fcec02ec610b8925f8c03a149ed6a39bfbe313
goo <goofofferson@gmail.com>
2020-04-01 09:14:26 -0400

    return all data

--- git.lisp
+++ git.lisp
@@ -6,8 +6,10 @@
 (defun git-update-all ()
   "Does a git pull with --recurse-submodules and update --remote.  Brings all submodules up to date"
   #+nil(eazy-process:exec `("git" "pull"))
-  (eazy-process:exec `("git" "pull" "--recurse-submodules"))
-  (eazy-process:exec `("git" "submodule" "update" "--remote"))
+  (append
+   (multiple-value-list (eazy-process:exec `("git" "pull" "--recurse-submodules")))
+   (multiple-value-list (eazy-process:exec `("git" "submodule" "update" "--remote")))
+   )
   )
 
 

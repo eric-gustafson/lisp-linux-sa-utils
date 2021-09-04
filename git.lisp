9494a821a2ce54cee7cebaa6f0455ebd58ea3eaa
goofofferson@gmail.com <goofofferson@gmail.com>
2021-09-04 18:33:02 -0400

    repository cleanup

--- git.lisp
+++ git.lisp
@@ -5,7 +5,7 @@
 
 (defun git-update-all ()
   "Does a git pull with --recurse-submodules and update --remote.  Brings all submodules up to date"
-  #+nil(eazy-process:exec `("git" "pull"))g
+  #+nil(eazy-process:exec `("git" "pull"))
   ;(uiop-shell:lshm "git pull --recurse-submodules && git submodule update --remote")
   (uiop-shell:&& `("git pull --recurse-submodules")
 		 `("git submodule update --remote"))
